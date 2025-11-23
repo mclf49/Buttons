15b1f8a232b53cf53b1b56ffae229a1200ed4de2
mclf49 <mclf49@educa.madrid.org>
2025-11-23 17:18:03 +0100

    added counter mechanic

--- script.js
+++ script.js
@@ -1,18 +1,25 @@
     const buttons = []
     var pressedButton;
+    const header = document.getElementById("header")
+    const scorer = document.getElementById("headerScore")
+
     var count = 0;
+    var score = 0;
+
+
+
     function createButton(){
+        let x = Math.random()*window.innerWidth
+        let y = Math.random()*window.innerHeight
 
         element = document.createElement("button"),      
-        element.style.position = "absolute"
     
+        //html attributes
         element.setAttribute("class","drag");
         element.setAttribute("id", `${count}`);
         element.setAttribute("onClick","createButton()")
-        element.innerText = "1"
+        
         document.body.append(element);
-        let x = Math.random()*window.innerWidth
-        let y = Math.random()*window.innerHeight
         element.style.left= x+"px"
         element.style.top= y+"px"
         console.log("botón creado en "+ x +"x "+y+"y");
@@ -21,23 +28,34 @@
             let index = e.target.attributes.id.value;
             
             if(buttons[pressedButton]==buttons[index]||!pressedButton){return}
-            
+
+            //PER BUTTON -> ON MERGE
             if(buttons[pressedButton].value == buttons[index].value){
+
+
                 document.body.removeChild(buttons[pressedButton].element)
+
+                if(score<buttons[index].value){
+                    console.log("the score is "+score);
+                     score=buttons[index].value}
+                actualizeScorer(score+1)
                 buttons[index].value += 1
                 buttons[index].element.innerText = buttons[index].value
             }
         })
         
-
+        let value = 0
+        element.innerText=value
         const button = {
             element:element,
-            value: 1,
+            value: value,
         }
+
         buttons[count]= button;
         count++;
     }
 
+    //Global events 
         document.addEventListener("mousedown", (e)=>{
             pressedButton = e.target.attributes.id.value
             console.log(pressedButton);
@@ -53,6 +71,7 @@
             buttons[pressedButton].element.style.left= x+"px"
             buttons[pressedButton].element.style.top= y+"px"
         })
+
         document.addEventListener("mouseup", (e)=>{
             if(!pressedButton){
                 return
@@ -60,4 +79,9 @@
             pressedButton = undefined
         })
 
+        function actualizeScorer(score) {
+            scorer.innerText=score + " / 1000";
+        }
+        //initialize game
+        actualizeScorer(0)
         createButton()
