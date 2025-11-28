800777fd3174bb99240b4521ea9c2342435107fc
mclf49 <mclf49@educa.madrid.org>
2025-11-28 17:53:20 +0100

    Added fontAwesome package and a top bar

--- script.js
+++ script.js
@@ -9,17 +9,22 @@
 
 
     function createButton(value){
+
         let x = Math.random()*window.innerWidth
         let y = Math.random()*window.innerHeight
+        
+        element = document.createElement("button");
+        
+        const button = {
+            element:element,
+            value: value,
+        }
 
-        element = document.createElement("button"),      
-    
         //html attributes
         element.setAttribute("class","drag");
         element.setAttribute("id", `${count}`);
         element.setAttribute("onClick",`createButton(${value})`)
         
-        document.body.append(element);
         element.style.left= x+"px"
         element.style.top= `${y}px`
         console.log("botón creado en "+ x +"x "+y+"y");
@@ -40,17 +45,22 @@
                      score=buttons[index].value}
                 actualizeScorer(score+1)
                 buttons[index].value += 1
-                buttons[index].element.innerText = buttons[index].value
+                //<i class="fa fa-number>
+                buttons[index].element.innerHTML="";
+                for(digit of buttons[index].value.toString().split("")){ 
+                    if(digit=="+"){digit="plus"}
+                    buttons[index].element.innerHTML += `<i class="fa-solid fa-${digit}"></i>`
+                }
                 buttons[index].element.setAttribute("onClick",`createButton(${buttons[index].value})`)
             }
         })
         
-        element.innerText=value
-        const button = {
-            element:element,
-            value: value,
+        for(digit of value.toString().split("")){      
+            if(digit=="+"){digit="plus"}
+            element.innerHTML+=`<i class="fa-solid fa-${digit}"></i>`;
         }
-
+        
+        document.body.append(element);
         buttons[count]= button;
         count++;
     }
@@ -80,8 +90,21 @@
         })
 
         function actualizeScorer(score) {
-            scorer.innerText=score + " / 1000";
+
+            scorer.innerHTML=""
+
+            for(digit of score.toString().split("")){      
+            if(digit=="+"){digit="plus"}
+            scorer.innerHTML+=`<i class="fa-solid fa-${digit}"></i>`;
         }
+
+            scorer.innerHTML+=`<i class="fa-solid fa-slash fa-flip-horizontal"></i>
+                               <i class="fa-solid fa-1"></i>
+                               <i class="fa-solid fa-0"></i>
+                               <i class="fa-solid fa-0"></i>
+                               <i class="fa-solid fa-0"></i>`;
+        }
+
         //initialize game
-        actualizeScorer(0)
-        createButton(0)
+        actualizeScorer(1)
+        createButton(1)
